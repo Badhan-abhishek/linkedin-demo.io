8812ef56db02961d11882efedb2d2c824d018bdb
Abhishek <badhanabhishek3@gmail.com>
2020-08-04 11:08:31 -0700

    New changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,61 +49,176 @@ Vue.component("content-main", {
     },
   },
   template: `
-    <div class="wrapper-content">
-      <div class="user-section">
-      <div class="user-img">
-        <img :src="image" class="image-user-left">
-        <span class="user-name"> {{this.current_user.name}} </span>
-        <span class="company-name"> {{this.current_user.company }} </span>
-      </div>
-      <div class="status">
-        <div class="connections">
+  <div class="wrapper-content">
+  <div class="user-section">
+    <div class="user-img">
+      <img :src="image" class="image-user-left" />
+      <span class="user-name"> {{this.current_user.name}} </span>
+      <span class="company-name"> {{this.current_user.company }} </span>
+    </div>
+    <div class="status">
+      <div class="connections">
         <span class="nums">{{this.current_user.connections}}</span>
         <span class="text">Connections</span>
-        </div>
-        <div class="views">
+      </div>
+      <div class="views">
         <span class="nums">{{this.current_user.views}}</span>
         <span class="text">views</span>
-        </div>
       </div>
-      <div class="links">
-        <div class="links-linkedin"> 
-        <i class="fab fa-linkedin-in"></i> <a :href="linkedinLink">{{linkedinName}}</a>
-        </div>
-        <div class="links-github">
+    </div>
+    <div class="links">
+      <div class="links-linkedin">
+        <i class="fab fa-linkedin-in"></i>
+        <a :href="linkedinLink">{{linkedinName}}</a>
+      </div>
+      <div class="links-github">
         <i class="fab fa-github-alt"></i>{{this.current_user.github}}
-        </div>
-        <div class="links-twitter">
+      </div>
+      <div class="links-twitter">
         <i class="fab fa-facebook-f"></i>{{this.current_user.twitter}}
+      </div>
+    </div>
+  </div>
+  <div class="center-section">
+    <div class="center-section-pseudo">
+      <div class="create-post">
+        <div class="create-options">
+          <button class="share-update btn-active">
+            <i class="fas fa-pen"></i> Share an update
+          </button>
+          <button class="uplaod-photo">
+            <i class="fas fa-image fa-1x"></i> Upload a photo
+          </button>
+          <button class="write-article">
+            <i class="fas fa-book-open"></i> Write an article
+          </button>
+        </div>
+        <div class="post-input">
+          <input type="text" placeholder="Write something..." />
         </div>
       </div>
-      </div>
-      <div class="center-section">
-        <div class="create-post">
-            <div class="create-options">
-                <button class="share-update btn-active">
-                <i class="fas fa-pen"></i> Share an update
-                </button>
-                <button class="uplaod-photo">
-                <i class="fas fa-image fa-1x"></i> Upload a photo
-                </button>
-                <button class="write-article">
-                <i class="fas fa-book-open"></i> Write an article
-                </button>
+      <div class="post-show">
+        <div v-for="(post, index) in posts" :key="post.id">
+          <div class="post-card">
+            <div class="card-head">
+              <div class="user-details">
+                <img :src="post.avatar" />
+                <div class="nameandc">
+                  <span class="post-name">{{post.name}}</span>
+                  <span class="post-company">{{post.company}}</span>
+                </div>
+              </div>
+              <div class="post-time">
+                {{post.time}}
+              </div>
             </div>
-            <div class="post-input">
-                <input type="text" placeholder="Write something..." >
+            <div class="card-body">
+              <div class="card-text">
+                {{post.text}}
+              </div>
+              <div class="card-img">
+                <div v-if="post.postImg.length !== 0">
+                  <img :src="post.postImg" />
+                </div>
+                <div v-else></div>
+              </div>
             </div>
+            <div class="card-footer">
+              <div class="reaction-panel">
+                <button v-on:click="inc(index)">
+                  <i class="far fa-heart"></i> {{post.likes}}
+                </button>
+                <button class="comments-count">
+                  <i class="far fa-comment"></i> {{post.comments}}
+                </button>
+                <button class="share-btn">
+                  <i class="fas fa-share-alt"></i>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="right-section">
-      <!-- c -->
-      </div>
-      </div>`,
+      <div class="extra-pseudo"></div>
+    </div>
+  </div>
+  <div class="right-section">
+    <div class="right-heading">
+       <button> Keep in touch </button>
+    </div>
+    <div v-for="user in users" >
+        <div class="user-details">
+            <img :src="user.avatar" />
+            <div class="nameandc">
+                  <span class="post-name">{{user.name}}</span>
+                  <span class="post-company">{{user.company}}</span>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+`,
   data() {
     return {
       name: "LinkedIn",
+      posts: [
+        {
+          id: 234,
+          name: "Monica Geller",
+          avatar:
+            "https://images.pexels.com/photos/157661/young-woman-shooting-model-157661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          company: "Facebook Inc",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis exercitationem corrupti sunt possimus, laborum adipisci modi amet ea rerum autem?",
+          postImg: "",
+          time: "Just Now",
+          likes: 0,
+          comments: 0,
+        },
+
+        {
+          id: 233,
+          name: "Abhishek",
+          avatar:
+            "https://images.pexels.com/photos/749091/pexels-photo-749091.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          company: "Alibaba LLC",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis exercitationem corrupti sunt possimus, laborum adipisci modi amet ea rerum autem?",
+          postImg:
+            "https://images.pexels.com/photos/208745/pexels-photo-208745.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          time: "2 Hours ago",
+          likes: 0,
+          comments: 0,
+        },
+      ],
+      users: [
+        {
+          id: 234,
+          name: "Monica Geller",
+          avatar:
+            "https://images.pexels.com/photos/157661/young-woman-shooting-model-157661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          company: "Facebook Inc",
+        },
+        {
+          id: 233,
+          name: "Abhishek",
+          avatar:
+            "https://images.pexels.com/photos/749091/pexels-photo-749091.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          company: "Alibaba LLC",
+        },
+      ],
     };
+  },
+  methods: {
+    inc: function (index) {
+      if (this.posts[index].likes === 1) {
+        this.posts[index].likes -= 1;
+      } else {
+        this.posts[index].likes += 1;
+      }
+    },
   },
   computed: {
     image() {
@@ -130,7 +245,7 @@ var app = new Vue({
     current_user: {
       name: "Abhishek",
       profile_picture:
-        "https://image.shutterstock.com/image-vector/male-default-avatar-profile-icon-600w-1725062341.jpg",
+        "https://images.pexels.com/photos/749091/pexels-photo-749091.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
       company: "Yahoo inc",
       connections: 346,
       views: 50,
